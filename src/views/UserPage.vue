f3fd7c0344455c7cb9d6a7cdab3d8c43add5ce40
Sittisak Seti <sittisak.st@mail.kmutt.ac.th>
2022-06-24 14:57:11 +0700

    feat: add back to user page

--- src/views/UserPage.vue
+++ src/views/UserPage.vue
@@ -2,23 +2,38 @@
   <div>
     <h1>Welcome {{ $store.getters.fullName(this.num) }}</h1>
     <hr />
+    <br />
+    <div class="my-button">
+      <atom-button :callBack="back">Back</atom-button>
+    </div>
   </div>
 </template>
 
 <script>
+import AtomButton from "@/components/atoms/AtomButton.vue";
 export default {
+  components: { AtomButton },
   name: "UserPage",
   data() {
     return {
       num: this.$store.state.myuser,
     };
   },
+  methods: {
+    back() {
+      this.$router.push("/");
+    },
+  },
 };
 </script>
 
-<style scope>
+<style scoped>
 h1 {
   text-align: center;
   color: white;
+}
+.my-button {
+  width: 100px;
+  margin: auto;
 }
 </style>
